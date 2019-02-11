d3bcf8c15a125b589c8682377f4666ef89f10227
fatal-zio <40217128+fatal-zio@users.noreply.github.com>
2019-02-11 15:28:36 -0600

    Added query argument and get by Id.

--- app.js
+++ app.js
@@ -8,11 +8,26 @@ const bookRouter = express.Router();
 const Book = require('./models/bookModel');
 
 bookRouter.route('/books').get((req, res) => {
-  Book.find((err, books) => {
+  const query = {};
+
+  if (req.query.genre) {
+    query.genre = req.query.genre;
+  }
+
+  Book.find(query, (err, books) => {
     if (err) {
       return res.send(err);
     }
     return res.json(books);
+  });
+});
+
+bookRouter.route('/books/:bookId').get((req, res) => {
+  Book.findById(req.params.bookId, (err, book) => {
+    if (err) {
+      return res.send(err);
+    }
+    return res.json(book);
   });
 });
 
