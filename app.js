73516ba8daa1a69d57a30c8d2e1c30431615eeec
fatal-zio <40217128+fatal-zio@users.noreply.github.com>
2019-02-11 16:54:13 -0600

    Implemented HTTP Put.

--- app.js
+++ app.js
@@ -14,10 +14,6 @@ app.use(bodyParser.json());
 
 app.use('/api', bookRouter);
 
-app.get('/', (req, res) => {
-  res.send('Welcome to my API!');
-});
-
 app.listen(port, () => {
   console.log(`Running on port ${port}.`);
 });
