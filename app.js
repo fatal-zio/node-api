baacdd731a992c9d1ac59f4313ffb3008cf8e748
fatal-zio <40217128+fatal-zio@users.noreply.github.com>
2019-02-11 15:16:36 -0600

    Added MongoDB integration. Implemented GET action.

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+const db = mongoose.connect('mongodb://localhost/bookAPI');
 const port = process.env.PORT || 3000;
 const bookRouter = express.Router();
+const Book = require('./models/bookModel');
 
 bookRouter.route('/books').get((req, res) => {
-  const response = { hello: 'This is my API' };
-  res.json(response);
+  Book.find((err, books) => {
+    if (err) {
+      return res.send(err);
+    }
+    return res.json(books);
+  });
 });
 
 app.use('/api', bookRouter);
