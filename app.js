79abf247a97bcd76ee5d8a0f52ece9b736c45bba
fatal-zio <40217128+fatal-zio@users.noreply.github.com>
2019-02-11 16:30:04 -0600

    Split router into its own file.

--- app.js
+++ app.js
@@ -6,43 +6,11 @@ const app = express();
 const db = mongoose.connect('mongodb://localhost/bookAPI');
 
 const port = process.env.PORT || 3000;
-const bookRouter = express.Router();
 const Book = require('./models/bookModel');
+const bookRouter = require('./routes/bookRouter')(Book);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-bookRouter
-  .route('/books')
-  .post((req, res) => {
-    const book = new Book(req.body);
-
-    book.save();
-    return res.status(201).json(book);
-  })
-  .get((req, res) => {
-    const query = {};
-
-    if (req.query.genre) {
-      query.genre = req.query.genre;
-    }
-
-    Book.find(query, (err, books) => {
-      if (err) {
-        return res.send(err);
-      }
-      return res.json(books);
-    });
-  });
-
-bookRouter.route('/books/:bookId').get((req, res) => {
-  Book.findById(req.params.bookId, (err, book) => {
-    if (err) {
-      return res.send(err);
-    }
-    return res.json(book);
-  });
-});
 
 app.use('/api', bookRouter);
 
